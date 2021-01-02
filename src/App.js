eec5055513374b0b2ab8540b9662ed73834c6325
rihem <rihemebenhassan@insat.u-carthage.tn>
2021-01-03 00:00:35 +0100

    methods as props (child component)

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Message from './Components/Message'
 import Counter from './Components/counter';
 import FunctionClick from './Components/FunctionClick';
 import EventBind from './Components/EventBind';
+import ParentComponent from './Components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
-      <EventBind/>
-       {/*<Counter/>
+     <ParentComponent/> 
+     {/*  <EventBind/>
+      <Counter/>
 
      <Message/>
 
---
+++ src/Components/ChildComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChildComponent(props) {
+    return (
+        <div>
+             <button onClick={props.greetHandler('child')}>Greet Parent</button>
+        </div>
+    )
+}
+
+export default ChildComponent
